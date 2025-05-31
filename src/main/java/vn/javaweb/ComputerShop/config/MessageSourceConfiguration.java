53d76ef8f0d1a6d2c70d8695c54b7f67ba1e1e63
MINAHAHA224 <144884804+MINAHAHA224@users.noreply.github.com>
2025-05-31 15:03:31 +0700

    Feat Internationalization (i18n) file config

---
+++ src/main/java/vn/javaweb/ComputerShop/config/MessageSourceConfiguration.java
@@ -0,0 +1,20 @@
+package vn.javaweb.ComputerShop.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class MessageSourceConfiguration {
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasenames("i18n/messages"); // base name of the resource bundle
+        source.setDefaultEncoding("UTF-8");
+        source.setUseCodeAsDefaultMessage(true);
+        source.setCacheSeconds(3600); // Refresh cache once every hour
+        return source;
+    }
+
+}
